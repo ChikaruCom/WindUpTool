52dd8886c52113de182c9a9a583a0402b2df16c3
ChikaruCom <mizukic@gmail.com>
2024-10-08 19:25:59 +0900

    Changes to be committed: 	modified:   1-install_wut.bat 	modified:   1-install_wut.py

--- 1-install_wut.py
+++ 1-install_wut.py
@@ -101,51 +101,7 @@ def print_interpreter_path():
     interpreter_path = sys.executable
     print(f"Python interpreter path: {interpreter_path}")
 
-def install_pip():
-    try:
-        import pip
-    except ImportError:
-        print("pip not found. Installing pip...")
-        subprocess.check_call([sys.executable, '-m', 'ensurepip', '--upgrade'])
-        subprocess.check_call([sys.executable, '-m', 'pip', 'install', '--upgrade', 'pip'])
-        print("pip installed successfully.")
-
-def install_and_import(module_name, version=None):
-    try:
-        if version:
-            module = importlib.import_module(module_name)
-            installed_version = module.__version__
-            if installed_version != version:
-                raise ImportError(f"Version mismatch: {installed_version} != {version}")
-        else:
-            importlib.import_module(module_name)
-    except ImportError:
-        if version:
-            package = f"{module_name}=={version}"
-        else:
-            package = module_name
-        print(f"{package} not found or version mismatch. Installing...")
-        subprocess.check_call([sys.executable, '-m', 'pip', 'install', package])
-        print(f"{package} installed successfully.")
-        importlib.import_module(module_name)
-
 def main():
-    # pipがインストールされているか確認し、インストール
-    install_pip()
-
-    # チェックするモジュールとバージョンのリスト
-    modules_to_check = [
-#        ("numpy", None),  # numpy
-        ("pandas", "2.2.2"),  # pandas
-        ("PyPDF2", "3.01"),  # PyPDF2
-        ("pywin32", "306"),  # win32com
-        ("psutil", "6.0.0"),
-        ("pyperclip", "1.9.0"),
-#        ("requests", None)  # requests
-    ]
-
-    for module, version in modules_to_check:
-        install_and_import(module, version)
 
     # Pythonインタープリタのパスを表示
     print(f"Python interpreter path: {sys.executable}")
